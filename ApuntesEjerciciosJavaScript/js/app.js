0f1a6429cb11734b27861c7543d33f7d3f673e2c
OscarJavierDosSantos <oscarjavierdos@gmail.com>
2024-05-28 09:30:40 +0200

    reorganizacion

---
+++ ApuntesEjerciciosJavaScript/js/app.js
@@ -0,0 +1,7 @@
+function diaDeHOy(){
+    let hoy= new Date();
+    console.log( hoy.toString());
+    document.write(hoy.toUTCString());//para escribir en HTML
+};
+
+alert("Bienvenido a The KuroLink´s armor Usuario");
