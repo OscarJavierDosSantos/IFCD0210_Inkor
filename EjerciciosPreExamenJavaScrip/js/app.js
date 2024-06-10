129669b72fd1ff854b00ed2b724379facad77ea3
OscarJavierDosSantos <oscarjavierdos@gmail.com>
2024-06-10 13:43:49 +0200

    Create app.js

---
+++ EjerciciosPreExamenJavaScrip/js/app.js
@@ -0,0 +1,44 @@
+/*Ejercicio 1*/
+// let precio=200;
+// let iva=21;
+// let precioFinal;
+
+// precioFinal=(precio+((precio*iva)/100));
+// console.log("El precio total es: "+precioFinal+" €");
+/*Fin Ejercico 1 */
+/*Ejercicio 2*/
+// let lado=40;
+// let area=lado*lado;
+// let perimetro=lado+lado+lado+lado;
+
+// console.log("El area es: "+area);
+// console.log("El perimetro es: "+perimetro);
+// /*Fin Ejercico 2 */
+// /*Ejercicio 3*/
+// let nombre=prompt("¿Cual es su nombre Sr/Sra?");
+// console.log("Hola "+ nombre.charAt(0).toUpperCase() + nombre.slice(1)+" espero que sea de su agrado la navegacion por la web");
+/*Fin Ejercico 3 */
+
+/*Ejercicio 4*/
+// let num1=parseInt(prompt("¿Primer numero?"));
+// let num2=parseInt(prompt("¿Segundo numero?"));
+// let num3=parseInt(prompt("¿Tercero numero?"));
+// let  media=(num1+num2+num3)/3;
+// alert("Los numeros elegidos son "+ num1+" , "+num2+" y "+num3+" y la media es :"+media);
+/*Fin Ejercico 4*/
+/*Ejercicio 5*/
+// let kilometros=parseInt(prompt("¿Numero de kilometros?"));
+// let combustible=parseInt(prompt("¿Litros de gasofa?"));
+// let combuXKilo = (combustible/kilometros);
+// alert("Son "+combuXKilo+ " litros por kilometro");
+/*Fin Ejercico 5*/
+/*Ejercicio 6*/
+let hora=parseInt(prompt("Dime las horas"));
+let min=parseInt(prompt("Dime los minutos"));
+const cambio=60;
+let seg=(hora*cambio*cambio)+(min*cambio);
+console.log("Dado los numero indicados "+hora+":"+min+"\nLa tranformacion a segundos es "+seg);
+/*Fin Ejercicio 6*/
+/*Ejercicio 7*/
+
+/*Fin Ejercicio 7*/
