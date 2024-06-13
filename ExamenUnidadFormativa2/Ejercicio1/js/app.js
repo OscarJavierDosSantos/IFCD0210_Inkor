5b67de2c03359847b865b53669b2f83456fcaa3f
OscarJavierDosSantos <oscarjavierdos@gmail.com>
2024-06-13 14:00:52 +0200

    examen

---
+++ ExamenUnidadFormativa2/Ejercicio1/js/app.js
@@ -0,0 +1,9 @@
+let gastoAlojamiento=200;
+let gastoAlimentacion=150;
+let gastoEntretenimiento=100;
+
+Gastos(gastoAlojamiento,gastoAlimentacion,gastoEntretenimiento);
+
+function Gastos(alojamiento,alimentacion,entretenimiento) {
+    alert("El coste total del viaje es "+(alojamiento+alimentacion+entretenimiento)+"€")
+}
--- ProyectoFutbol/js/main.js
+++ ProyectoFutbol/js/main.js
@@ -1,17 +1,25 @@
-function enviarForm(nombre,email,asusto,mensaje){
+document.getElementById('nombre').value,
+                                                document.getElementById('apellido').value,
+                                                document.getElementById('pass').value,
+                                                document.getElementById('sexo').value,
+                                                document.getElementById('email').value
+function enviarForm(nombre,apellido,pass,sexo,email){
     let mensajeFinal="A continuacion te indico los campos a rellenar\n";
     //Para comprobar los distintos campos a validar
     let nombreComp=comprobar(nombre);
+    let apeComp=comprobar(apellido);
+    let passComp=comprobar(pass);
+    let sexoComp=comprobar(sexo);
     let emailComp=comprobar(email);
-    let asuntoComp=comprobar(asusto);
-    let mensajeComp=comprobar(mensaje);
+    
     
     if (nombreComp==true) {mensajeFinal+="Te falta por incluir el Nombre\n"; }
+    if (apeComp==true) {mensajeFinal+="Te falta por incluir el Apellido\n"; }
+    if (passComp==true) {mensajeFinal+="Te falta por incluir el Password\n"; }
+    if (sexoComp==true) {mensajeFinal+="Te falta por incluir el Sexo\n"; } 
     if (emailComp==true) {mensajeFinal+="Te falta por incluir el Email\n";  }
-    if (asuntoComp==true) {mensajeFinal+="Te falta por incluir el Asunto\n"; } 
-    if (mensajeComp==true) {mensajeFinal+="Te falta por incluir el Mensaje\n";} 
 
-    if ( nombreComp==false && emailComp==false && asuntoComp==false && mensajeComp==false){
+    if ( nombreComp==false && apeComp==false && emailComp==false && passComp==false && sexoComp==false && mensajeComp==false){
             mensajeFinal=("Gracias por contactar con nosotros "+nombre.toUpperCase() + 
             "\nLeeremos su mensaje y le responderemos los antes posible ") ;    
         }
